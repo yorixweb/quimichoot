73f2632e699a0e114d5cc93e313a6ddb6fb61ed6
Johan <johan@example.com>
2026-09-14 23:59:56 -0500

    Conectar Firebase

--- js/config.js
+++ js/config.js
@@ -1,11 +1,11 @@
 export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyAmCU80NgtOkJuxVmlRpf8isoKlXGzveBA",
+  authDomain: "quimichool.firebaseapp.com",
+  databaseURL: "https://quimichool-default-rtdb.firebaseio.com",
+  projectId: "quimichool",
+  storageBucket: "quimichool.firebasestorage.app",
+  messagingSenderId: "420754755153",
+  appId: "1:420754755153:web:a72efa174d3a5d41e7b270"
 };
 
 export const MAX_PLAYERS = 50;
